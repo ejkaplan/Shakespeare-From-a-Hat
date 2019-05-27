335adb623125fc7804875d3bd35f13e901146e5d
Eliot J. Kaplan <kaplan.eliotj@gmail.com>
2019-05-27 15:32:37 -0400

    processing integrate

---
+++ Ro-Bard/src/visual/BardVis.java
@@ -0,0 +1,23 @@
+package visual;
+
+import processing.core.PApplet;
+
+public class BardVis extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("visual.BardVis");
+	}
+
+	public void settings() {
+		size(640, 480);
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+
+	}
+
+}
